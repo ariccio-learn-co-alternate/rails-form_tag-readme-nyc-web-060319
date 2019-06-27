e1ac4a41504efc44d194df4b749d2071dce68e28
Alexander Riccio <test35965+learn@gmail.com>
2019-06-27 16:19:44 -0400

    solve lab

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def new
+  def create
     Post.create(title: params[:post][:title], description: params[:post][:description])
     redirect_to posts_path
   end
